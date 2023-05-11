ce41412128f647b6b1953cbd6e13b155dbed23fb
denys.stefanko <partner.denys.stefanko@philips.com>
2023-05-11 11:44:10 +0300

    initial commit

---
+++ eureka/src/test/java/brewery/eureka/breweryeureka/BreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package brewery.eureka.breweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
